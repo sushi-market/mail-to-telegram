402acb8a8334c86a81d5afde53fd869270f663d7
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2020-07-31 12:26:38 +0300

    Disable pretty tables

--- processing.go
+++ processing.go
@@ -89,7 +89,7 @@ func splitLongLine(line string, limit int) []string {
 }
 
 func MailBodyProcessing(msg string) string {
-	text, err := html2text.FromString(msg, html2text.Options{PrettyTables: true})
+	text, err := html2text.FromString(msg)
 	if err != nil {
 		log.Println("failed to do html2text: ", err)
 		return msg
